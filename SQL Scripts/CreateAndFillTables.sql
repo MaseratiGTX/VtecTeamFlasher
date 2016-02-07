52552aafa9382da6fabad212439418fe8b2f765a
chiganock <emoskalenko@neoservice.kg>
2016-02-07 19:22:14 +0600

    * Добавил все таблицы в скрипт

--- SQL Scripts/CreateAndFillTables.sql
+++ SQL Scripts/CreateAndFillTables.sql
@@ -51,5 +51,130 @@ GO
 
 
 
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_ReflashHistory_Users]') AND parent_object_id = OBJECT_ID(N'[dbo].[ReflashHistory]'))
+ALTER TABLE [dbo].[ReflashHistory] DROP CONSTRAINT [FK_ReflashHistory_Users]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ReflashHistory]') AND type in (N'U'))
+DROP TABLE [dbo].[ReflashHistory]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ReflashHistory](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[ReslashFileName] [nvarchar](255) NOT NULL,
+	[Vin] [nvarchar](255) NOT NULL,
+	[Cvn] [nvarchar](255) NOT NULL,
+	[StockCalibration] [nvarchar](255) NOT NULL,
+	[ReflashStatus] [int] NOT NULL,
+	[PaymentStatus] [int] NOT NULL,
+	[ReflashDateTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_ReflashHistory] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[ReflashHistory]  WITH CHECK ADD  CONSTRAINT [FK_ReflashHistory_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[ReflashHistory] CHECK CONSTRAINT [FK_ReflashHistory_Users]
+GO
+
+
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Requests_Users]') AND parent_object_id = OBJECT_ID(N'[dbo].[Requests]'))
+ALTER TABLE [dbo].[Requests] DROP CONSTRAINT [FK_Requests_Users]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Requests]') AND type in (N'U'))
+DROP TABLE [dbo].[Requests]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Requests](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[StockFile] [varbinary](max) NULL,
+	[EcuCode] [nvarchar](50) NOT NULL,
+	[RequestStatus] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[RequestDateTime] [datetime] NOT NULL,
+	[AdditionalMessage] [nvarchar](255) NULL,
+ CONSTRAINT [PK_Requests] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Requests]  WITH CHECK ADD  CONSTRAINT [FK_Requests_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[Requests] CHECK CONSTRAINT [FK_Requests_Users]
+GO
+
+
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Review_ReflashHistory]') AND parent_object_id = OBJECT_ID(N'[dbo].[Review]'))
+ALTER TABLE [dbo].[Review] DROP CONSTRAINT [FK_Review_ReflashHistory]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Review]') AND type in (N'U'))
+DROP TABLE [dbo].[Review]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Review](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ReflashHistoryId] [int] NOT NULL,
+	[UserName] [nvarchar](50) NULL,
+	[SourceUrl] [nvarchar](150) NULL,
+	[UserReview] [nvarchar](max) NOT NULL,
+	[ReviewDateTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_Review] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Review]  WITH CHECK ADD  CONSTRAINT [FK_Review_ReflashHistory] FOREIGN KEY([ReflashHistoryId])
+REFERENCES [dbo].[ReflashHistory] ([Id])
+GO
+
+ALTER TABLE [dbo].[Review] CHECK CONSTRAINT [FK_Review_ReflashHistory]
+GO
+
+
+
+
 
 
